60d17746e3465d499f1749f98aca1621f89d61fd
renaldin <renaldinoviandi0@gmail.com>
2023-04-09 14:56:57 +0700

    add api and display latest post

--- components/section2.js
+++ components/section2.js
@@ -1,20 +1,26 @@
 import Link from "next/link"
 import Image from "next/image"
 import Author from "./_child/author"
+import fetcher from "@/lib/fetcher"
 
 const Section2 = () => {
+
+    const { data, isLoading, isError } = fetcher('api/posts')
+
+    if (isLoading) return <div>Loading...</div>
+    if (isError) return <div>Error</div>
+
     return (
         <section className="container mx-auto md:px-20 py-10">
             <h1 className="font-bold text-4xl py-12 text-center">Latest Posts</h1>
 
             {/* grid columns */}
             <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-14">
-                {Post()}
-                {Post()}
-                {Post()}
-                {Post()}
-                {Post()}
-                {Post()}
+                {
+                    data.map((value, index) => (
+                        <Post data={value} key={index}></Post>
+                    ))
+                }
             </div>
         </section>
     )
@@ -22,26 +28,27 @@ const Section2 = () => {
 
 export default Section2
 
-function Post() {
+function Post({ data }) {
+    const { id, title, subtitle, category, img, published, author } = data;
     return (
         <div className="item">
             <div className="images">
                 <Link legacyBehavior href={"/"}>
-                    <a className="rounded"><Image src={"/images/img1.jpg"} width={500} height={350} /></a>
+                    <a className="rounded"><Image src={img || "/"} width={500} height={350} /></a>
                 </Link>
             </div>
             <div className="info flex justify-center flex-col py-4">
                 <div className="cat">
-                    <Link legacyBehavior href={"/"}><a className="text-orange-600 hover:text-orange-800">Business, Travel</a></Link>
-                    <Link legacyBehavior href={"/"}><a className="text-gray-800 hover:text-gray-600"> - July 3, 2023</a></Link>
+                    <Link legacyBehavior href={"/"}><a className="text-orange-600 hover:text-orange-800">{category || 'Unknown'}</a></Link>
+                    <Link legacyBehavior href={"/"}><a className="text-gray-800 hover:text-gray-600">- {published || "Unknown"}</a></Link>
                 </div>
                 <div className="title">
-                    <Link legacyBehavior href={"/"}><a className="text-xl font-bold text-gray-800 hover:text-gray-600">Your most unhappy customers are your greatest source og learning</a></Link>
+                    <Link legacyBehavior href={"/"}><a className="text-xl font-bold text-gray-800 hover:text-gray-600">{title || "Title"}</a></Link>
                 </div>
                 <p className="text-gray-500 py-3">
-                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Iusto, maiores veritatis? Debitis nostrum eum illo quae aperiam, culpa soluta cum laudantium esse veniam maiores mollitia veritatis ex nulla aliquid sint!
+                    {subtitle || 'Subtitle'}
                 </p>
-                <Author />
+                {author ? <Author /> : <></>}
             </div>
         </div>
     )
---
+++ pages/api/posts/[postId].js
@@ -0,0 +1,13 @@
+import data from "../data";
+
+export default function handler(req, res) {
+    const { postId } = req.query;
+    const { Posts } = data;
+
+    if (postId) {
+        const post = Posts.find(value => value.id == postId)
+        return res.status(200).json(post)
+    }
+
+    return res.status(404).json({ error: "Data Not Found" })
+}
